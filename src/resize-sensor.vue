9e0fa97bccc63dfc6b79ab78144d8d5b6db13749
Franck Freiburger <franck.freiburger@gmail.com>
2017-04-13 16:58:44 +0200

    - replace <template> by vm.render function

--- src/resize-sensor.vue
+++ src/resize-sensor.vue
@@ -1,7 +1,3 @@
-<template>
-	<div @animationstart="update"><div @scroll="update"><div></div></div><div @scroll="update"><div></div></div></div>
-</template>
-
 <style>
 @keyframes resizeSensorVisibility {
 	from { top: 0; }
@@ -45,15 +41,48 @@ module.exports = {
 			this.reset();
 		}
 	},
+	render: function(create) {
+		
+		var vm = this;
+		
+		var style = 'position: absolute; left: 0; top: 0; right: 0; bottom: 0; overflow: hidden; z-index: -1; visibility: hidden; animation-name: resizeSensorVisibility;';
+		var styleChild = 'position: absolute; left: 0; top: 0;';
+
+		return create('div', {
+			style: style,
+			on: {
+				'~animationstart': vm.update.bind(vm),
+			}
+		},[
+			create('div', {
+				style: style,
+				on: {
+					scroll: vm.update.bind(vm)
+				}
+			}, [
+				create('div', {
+					style: styleChild + ' width: 100000px; height: 100000px;'
+				})
+			]),
+			create('div', {
+				style: style,
+				on: {
+					scroll: vm.update.bind(vm)
+				}
+			}, [
+				create('div', {
+					style: styleChild + ' width: 200%; height: 200%;'
+				})
+			]),
+		]);
+		
+	},
 	mounted: function() {
 		
 		this.afci = 0; // https://html.spec.whatwg.org/multipage/webappapis.html#animation-frame-callback-identifier
-		
-		var style = 'position: absolute; left: 0; top: 0; right: 0; bottom: 0; overflow: hidden; z-index: -1; visibility: hidden; animation-name: resizeSensorVisibility';
-		var styleChild = 'position: absolute; left: 0; top: 0;';
-		
+	
 		if ( !('AnimationEvent' in window) ) {
-			
+
 			var poll = function() {
 				
 				window.setTimeout(function() {
@@ -67,21 +96,14 @@ module.exports = {
 			poll();
 		}
 
-		this.$el.style.cssText = style;
-		
 		if ( this.$el.offsetParent !== this.$el.parentNode )
 			this.$el.parentNode.style.position = 'relative';
-		
-		var expand = this.$el.childNodes[0];
-		expand.style.cssText = style;
-		expand.firstChild.style.cssText = styleChild + ' width: 100000px; height: 100000px;';
-		
-		var shrink = this.$el.childNodes[1];
-		shrink.style.cssText = style;
-		shrink.firstChild.style.cssText = styleChild + ' width: 200%; height: 200%;';
-		
+
 		this.lastSize = { width: -1, height: -1 };
-		
+
+		var expand = this.$el.childNodes[0];
+		var shrink = this.$el.childNodes[1];
+
 		this.reset = function() {
 
 			expand.scrollLeft = 100000;
